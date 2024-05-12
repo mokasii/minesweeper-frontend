3f630ae9329de282c246d5067e31ff61f6677955
Mohamed Khalid Hassan Siddig <msiddig@outlook.de>
2024-05-12 20:51:03 +0200

    add game view

---
+++ src/views/GameView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="game">
+    <h1>This will be the game one day</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+<script setup lang="ts">
+</script>
