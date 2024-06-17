019e188cd3b3fbe46347984cfca24282267be363
Mohamed Khalid Hassan Siddig <msiddig@outlook.de>
2024-06-17 23:13:59 +0200

    configure routing path for scoreForm

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import GameView from '../views/GameView.vue'
+import ScoreForm from '@/components/ScoreForm.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/game',
       name: 'game',
       component: GameView
+    },
+    {
+      path: '/score',
+      name: 'score',
+      component: ScoreForm
     }
   ]
 })
