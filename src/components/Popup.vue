64e8fd110012204846dbe03906a35b207411d33c
Mohamed Khalid Hassan Siddig <msiddig@outlook.de>
2024-06-26 15:02:00 +0200

    create Popup component

---
+++ src/components/Popup.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+
+const props = defineProps({
+  show: Boolean
+});
+
+const emit = defineEmits(['close']);
+
+const close = () => {
+  emit('close');
+};
+</script>
+
+<template>
+  <div class="popup" v-if="show">
+    <div class="popup-content">
+      <slot></slot>
+      <button @click="close">Close</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.popup {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.2);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.popup-content {
+  background-color: grey;
+  padding: 20px;
+  border-radius: 10px;
+}
+</style>
