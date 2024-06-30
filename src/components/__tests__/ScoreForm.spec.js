e13e0b4887eadb367e9c34eef0aff83b545776d1
Mohamed Khalid Hassan Siddig <msiddig@outlook.de>
2024-07-01 00:11:46 +0200

    fix typos remove unused stuff

--- src/components/__tests__/ScoreForm.spec.js
+++ src/components/__tests__/ScoreForm.spec.js
@@ -1,18 +1,8 @@
 import { describe, it, expect, vi } from 'vitest'
-import { shallowMount, flushPromises } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import ScoreForm from '@/components/ScoreForm.vue'
-import axios from 'axios'
-import api from '../../api/api'
-import type { Score } from '../../types/scoreType'
-
 
 describe('ScoreForm.vue', () => {
-
-  const emptyResponse: Score[] = []
-  const twoItemResponse: Score[] = [
-    { "id": 1738, "nickname": "dkjng", "timeInSeconds": 2, "difficulty": "easy", "createdAt": "2015-06-27T16:21:51.453306" },
-    { "id": 2410, "nickname": "gfhmgh", "timeInSeconds": 3, "difficulty": "medium", "createdAt": "2016-06-27T16:21:51.453306" }
-  ]
 
   it('renders timeInSeconds and difficulty passed to it', async () => {
     const time = 100;
