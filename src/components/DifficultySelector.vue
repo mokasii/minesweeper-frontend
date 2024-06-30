5ae2c7ba7260e86f3474f6e17a50b1de59b64b09
Mohamed Khalid Hassan Siddig <msiddig@outlook.de>
2024-07-01 00:25:21 +0200

    add audio to diffic buttons

--- src/components/DifficultySelector.vue
+++ src/components/DifficultySelector.vue
@@ -5,10 +5,21 @@ const emit = defineEmits(['difficultySelected']);
 function emitDifficulty(level: string) {
   emit('difficultySelected', level);
 }
+
+//sound
+//sound
+import hoverSfx from "@/assets/sounds/tk3-buttonHover.mp3";
+import selectSfx from "@/assets/sounds/buttonSelect.mp3";
+
+const hoverAudio = new Audio(hoverSfx);
+const selectAudio = new Audio(selectSfx);
+
+const playHover = () => {hoverAudio.play();};
+
 </script>
 
 <template>
-  <div class="diffic-buttons">
+  <div class="diffic-buttons" @click="playHover">
     <button @click="emitDifficulty('easy')">Easy</button>
     <button @click="emitDifficulty('medium')">Medium</button>
     <button @click="emitDifficulty('hard')">Hard</button>
