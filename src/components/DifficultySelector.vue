11e97432e3663aa7c0feaa883462b36ec0b63b85
Mohamed Khalid Hassan Siddig <msiddig@outlook.de>
2024-07-01 13:43:29 +0200

    use global sound3

--- src/components/DifficultySelector.vue
+++ src/components/DifficultySelector.vue
@@ -6,20 +6,10 @@ function emitDifficulty(level: string) {
   emit('difficultySelected', level);
 }
 
-//sound
-//sound
-import hoverSfx from "@/assets/sounds/tk3-buttonHover.mp3";
-import selectSfx from "@/assets/sounds/buttonSelect.mp3";
-
-const hoverAudio = new Audio(hoverSfx);
-const selectAudio = new Audio(selectSfx);
-
-const playHover = () => {hoverAudio.play();};
-
 </script>
 
 <template>
-  <div class="diffic-buttons" @click="playHover">
+  <div class="diffic-buttons" @click="$sounds.playSound3()">
     <button @click="emitDifficulty('easy')">Easy</button>
     <button @click="emitDifficulty('medium')">Medium</button>
     <button @click="emitDifficulty('hard')">Hard</button>
