919b44c341047989b57dda70271495a901106508
Mohamed Khalid Hassan Siddig <msiddig@outlook.de>
2024-07-01 13:31:44 +0200

    use sounds globally

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import soundPlugin from './plugins/audioPlugin'
 
 const pinia = createPinia()
 const app = createApp(App)
 
 app.use(router)
 app.use(pinia)
+app.use(soundPlugin)
 
 app.mount('#app')
---
+++ src/plugins/audioPlugin.ts
@@ -0,0 +1,53 @@
+import hoverSfx from "@/assets/sounds/tk3-buttonHover.mp3";
+import selectSfx from "@/assets/sounds/buttonSelect.mp3";
+import sound2 from "@/assets/sounds/sound2.mp3";
+import sound3 from "@/assets/sounds/sound3.mp3";
+import sound4 from "@/assets/sounds/sound4.mp3";
+import sound5 from "@/assets/sounds/sound5.mp3";
+import type { App } from 'vue'
+
+const hoverAudio = new Audio(hoverSfx);
+const selectAudio = new Audio(selectSfx);
+const sound2Audio = new Audio(sound2);
+const sound3Audio = new Audio(sound3);
+const sound4Audio = new Audio(sound4);
+const sound5Audio = new Audio(sound5);
+
+const playHover = () => { hoverAudio.play(); };
+const stopHover = () => { hoverAudio.load(); }; // reloads audio
+
+const playSelect = () => { selectAudio.play(); };
+const stopSelect = () => { selectAudio.load(); }; // reloads audio
+
+const playSound2 = () => { sound2Audio.play(); };
+const stopSound2 = () => { sound2Audio.load(); }; // reloads audio
+
+const playSound3 = () => { sound3Audio.play(); };
+const stopSound3 = () => { sound3Audio.load(); }; // reloads audio
+
+const playSound4 = () => { sound4Audio.play(); };
+const stopSound4 = () => { sound4Audio.load(); }; // reloads audio
+
+const playSound5 = () => { sound5Audio.play(); };
+const stopSound5 = () => { sound5Audio.load(); }; // reloads audio
+
+const soundPlugin = {
+  install(app: App) {
+    app.config.globalProperties.$sounds = {
+      playHover,
+      stopHover,
+      playSelect,
+      stopSelect,
+      playSound2,
+      stopSound2,
+      playSound3,
+      stopSound3,
+      playSound4,
+      stopSound4,
+      playSound5,
+      stopSound5,
+    };
+  }
+};
+
+export default soundPlugin;
