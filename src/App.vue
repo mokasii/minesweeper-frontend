3a46de4525d064b874947b67aa4817b6b53195ce
Mohamed Khalid Hassan Siddig <msiddig@outlook.de>
2024-07-01 11:22:47 +0200

    route transitions

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView class="router-view" />
+  <RouterView class="router-view" v-slot="{Component}">
+    <transition name="route" mode="out-in">
+      <component :is="Component"></component>
+    </transition>
+  </RouterView>
 </template>
 
 <style scoped>
@@ -11,4 +15,21 @@ import { RouterView } from 'vue-router'
   width: 100%;
   min-height: 100vh;
 }
+
+/* route transitions */
+.route-enter-from {
+  opacity: 0;
+  transform: translateY(100px);
+}
+.route-enter-active {
+  transition: all 0.3s ease-out;
+}
+.route-leave-to {
+  opacity: 0;
+  transform: translateY(-100px);
+}
+.route-leave-active {
+  transition: all 0.3s ease-in;
+}
+
 </style>
